0cc3b534c18185d49a68cf0f4fc43cd2c4b912ea
Jean-Claude Ndagijimana <jeclauded@gmail.com>
2020-10-30 04:11:29 +1030

    for saving

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '/App.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
